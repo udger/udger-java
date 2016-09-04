a6b49e6bd1bf4983c3fadfd4768badda3c772198
skybber <lada.dvorak7@gmail.com>
2016-09-04 16:45:21 +0200

    Tests updated

--- src/test/java/org/udger/parser/UdgerUaTest.java
+++ src/test/java/org/udger/parser/UdgerUaTest.java
@@ -16,7 +16,7 @@ public class UdgerUaTest {
         JsonArray ja = jr.readArray();
         UdgerParser up = null;
         try {
-            up = new UdgerParser("/home/lada/Download/udgerdb_v3.dat");
+            up = new UdgerParser("udgerdb_v3.dat");
             for (int i=0; i < ja.size(); i++) {
                 JsonObject jar = ja.getJsonObject(i);
                 JsonObject jor = jar.getJsonObject("ret");
