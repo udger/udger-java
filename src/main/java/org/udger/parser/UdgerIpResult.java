36ea7421c7c50acf0561fcbf367999d133fe4898
skybber <lada.dvorak7@gmail.com>
2017-04-03 19:46:52 +0200

    toString(), hashCode(), equals() methods added

--- src/main/java/org/udger/parser/UdgerIpResult.java
+++ src/main/java/org/udger/parser/UdgerIpResult.java
@@ -210,4 +210,215 @@ public class UdgerIpResult implements Serializable {
         this.dataCenterHomePage = dataCenterHomePage;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((crawlerCategory == null) ? 0 : crawlerCategory.hashCode());
+        result = prime * result + ((crawlerCategoryCode == null) ? 0 : crawlerCategoryCode.hashCode());
+        result = prime * result + ((crawlerFamily == null) ? 0 : crawlerFamily.hashCode());
+        result = prime * result + ((crawlerFamilyCode == null) ? 0 : crawlerFamilyCode.hashCode());
+        result = prime * result + ((crawlerFamilyHomepage == null) ? 0 : crawlerFamilyHomepage.hashCode());
+        result = prime * result + ((crawlerFamilyIcon == null) ? 0 : crawlerFamilyIcon.hashCode());
+        result = prime * result + ((crawlerFamilyInfoUrl == null) ? 0 : crawlerFamilyInfoUrl.hashCode());
+        result = prime * result + ((crawlerFamilyVendor == null) ? 0 : crawlerFamilyVendor.hashCode());
+        result = prime * result + ((crawlerFamilyVendorCode == null) ? 0 : crawlerFamilyVendorCode.hashCode());
+        result = prime * result + ((crawlerFamilyVendorHomepage == null) ? 0 : crawlerFamilyVendorHomepage.hashCode());
+        result = prime * result + ((crawlerLastSeen == null) ? 0 : crawlerLastSeen.hashCode());
+        result = prime * result + ((crawlerName == null) ? 0 : crawlerName.hashCode());
+        result = prime * result + ((crawlerRespectRobotstxt == null) ? 0 : crawlerRespectRobotstxt.hashCode());
+        result = prime * result + ((crawlerVer == null) ? 0 : crawlerVer.hashCode());
+        result = prime * result + ((crawlerVerMajor == null) ? 0 : crawlerVerMajor.hashCode());
+        result = prime * result + ((dataCenterHomePage == null) ? 0 : dataCenterHomePage.hashCode());
+        result = prime * result + ((dataCenterName == null) ? 0 : dataCenterName.hashCode());
+        result = prime * result + ((dataCenterNameCode == null) ? 0 : dataCenterNameCode.hashCode());
+        result = prime * result + ((ip == null) ? 0 : ip.hashCode());
+        result = prime * result + ((ipCity == null) ? 0 : ipCity.hashCode());
+        result = prime * result + ((ipClassification == null) ? 0 : ipClassification.hashCode());
+        result = prime * result + ((ipClassificationCode == null) ? 0 : ipClassificationCode.hashCode());
+        result = prime * result + ((ipCountry == null) ? 0 : ipCountry.hashCode());
+        result = prime * result + ((ipCountryCode == null) ? 0 : ipCountryCode.hashCode());
+        result = prime * result + ((ipHostname == null) ? 0 : ipHostname.hashCode());
+        result = prime * result + ((ipLastSeen == null) ? 0 : ipLastSeen.hashCode());
+        result = prime * result + ipVer;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UdgerIpResult other = (UdgerIpResult) obj;
+        if (crawlerCategory == null) {
+            if (other.crawlerCategory != null)
+                return false;
+        } else if (!crawlerCategory.equals(other.crawlerCategory))
+            return false;
+        if (crawlerCategoryCode == null) {
+            if (other.crawlerCategoryCode != null)
+                return false;
+        } else if (!crawlerCategoryCode.equals(other.crawlerCategoryCode))
+            return false;
+        if (crawlerFamily == null) {
+            if (other.crawlerFamily != null)
+                return false;
+        } else if (!crawlerFamily.equals(other.crawlerFamily))
+            return false;
+        if (crawlerFamilyCode == null) {
+            if (other.crawlerFamilyCode != null)
+                return false;
+        } else if (!crawlerFamilyCode.equals(other.crawlerFamilyCode))
+            return false;
+        if (crawlerFamilyHomepage == null) {
+            if (other.crawlerFamilyHomepage != null)
+                return false;
+        } else if (!crawlerFamilyHomepage.equals(other.crawlerFamilyHomepage))
+            return false;
+        if (crawlerFamilyIcon == null) {
+            if (other.crawlerFamilyIcon != null)
+                return false;
+        } else if (!crawlerFamilyIcon.equals(other.crawlerFamilyIcon))
+            return false;
+        if (crawlerFamilyInfoUrl == null) {
+            if (other.crawlerFamilyInfoUrl != null)
+                return false;
+        } else if (!crawlerFamilyInfoUrl.equals(other.crawlerFamilyInfoUrl))
+            return false;
+        if (crawlerFamilyVendor == null) {
+            if (other.crawlerFamilyVendor != null)
+                return false;
+        } else if (!crawlerFamilyVendor.equals(other.crawlerFamilyVendor))
+            return false;
+        if (crawlerFamilyVendorCode == null) {
+            if (other.crawlerFamilyVendorCode != null)
+                return false;
+        } else if (!crawlerFamilyVendorCode.equals(other.crawlerFamilyVendorCode))
+            return false;
+        if (crawlerFamilyVendorHomepage == null) {
+            if (other.crawlerFamilyVendorHomepage != null)
+                return false;
+        } else if (!crawlerFamilyVendorHomepage.equals(other.crawlerFamilyVendorHomepage))
+            return false;
+        if (crawlerLastSeen == null) {
+            if (other.crawlerLastSeen != null)
+                return false;
+        } else if (!crawlerLastSeen.equals(other.crawlerLastSeen))
+            return false;
+        if (crawlerName == null) {
+            if (other.crawlerName != null)
+                return false;
+        } else if (!crawlerName.equals(other.crawlerName))
+            return false;
+        if (crawlerRespectRobotstxt == null) {
+            if (other.crawlerRespectRobotstxt != null)
+                return false;
+        } else if (!crawlerRespectRobotstxt.equals(other.crawlerRespectRobotstxt))
+            return false;
+        if (crawlerVer == null) {
+            if (other.crawlerVer != null)
+                return false;
+        } else if (!crawlerVer.equals(other.crawlerVer))
+            return false;
+        if (crawlerVerMajor == null) {
+            if (other.crawlerVerMajor != null)
+                return false;
+        } else if (!crawlerVerMajor.equals(other.crawlerVerMajor))
+            return false;
+        if (dataCenterHomePage == null) {
+            if (other.dataCenterHomePage != null)
+                return false;
+        } else if (!dataCenterHomePage.equals(other.dataCenterHomePage))
+            return false;
+        if (dataCenterName == null) {
+            if (other.dataCenterName != null)
+                return false;
+        } else if (!dataCenterName.equals(other.dataCenterName))
+            return false;
+        if (dataCenterNameCode == null) {
+            if (other.dataCenterNameCode != null)
+                return false;
+        } else if (!dataCenterNameCode.equals(other.dataCenterNameCode))
+            return false;
+        if (ip == null) {
+            if (other.ip != null)
+                return false;
+        } else if (!ip.equals(other.ip))
+            return false;
+        if (ipCity == null) {
+            if (other.ipCity != null)
+                return false;
+        } else if (!ipCity.equals(other.ipCity))
+            return false;
+        if (ipClassification == null) {
+            if (other.ipClassification != null)
+                return false;
+        } else if (!ipClassification.equals(other.ipClassification))
+            return false;
+        if (ipClassificationCode == null) {
+            if (other.ipClassificationCode != null)
+                return false;
+        } else if (!ipClassificationCode.equals(other.ipClassificationCode))
+            return false;
+        if (ipCountry == null) {
+            if (other.ipCountry != null)
+                return false;
+        } else if (!ipCountry.equals(other.ipCountry))
+            return false;
+        if (ipCountryCode == null) {
+            if (other.ipCountryCode != null)
+                return false;
+        } else if (!ipCountryCode.equals(other.ipCountryCode))
+            return false;
+        if (ipHostname == null) {
+            if (other.ipHostname != null)
+                return false;
+        } else if (!ipHostname.equals(other.ipHostname))
+            return false;
+        if (ipLastSeen == null) {
+            if (other.ipLastSeen != null)
+                return false;
+        } else if (!ipLastSeen.equals(other.ipLastSeen))
+            return false;
+        if (ipVer != other.ipVer)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "UdgerIpResult [" +
+                "ip=" + ip +
+                ", ipVer=" + ipVer +
+                ", ipClassification=" + ipClassification +
+                ", ipClassificationCode=" + ipClassificationCode +
+                ", ipLastSeen=" + ipLastSeen +
+                ", ipHostname=" + ipHostname +
+                ", ipCountry=" + ipCountry +
+                ", ipCountryCode=" + ipCountryCode +
+                ", ipCity=" + ipCity +
+                ", crawlerName=" + crawlerName +
+                ", crawlerVer=" + crawlerVer +
+                ", crawlerVerMajor=" + crawlerVerMajor +
+                ", crawlerFamily=" + crawlerFamily +
+                ", crawlerFamilyCode=" + crawlerFamilyCode +
+                ", crawlerFamilyHomepage=" + crawlerFamilyHomepage +
+                ", crawlerFamilyVendor=" + crawlerFamilyVendor +
+                ", crawlerFamilyVendorCode=" + crawlerFamilyVendorCode +
+                ", crawlerFamilyVendorHomepage=" + crawlerFamilyVendorHomepage +
+                ", crawlerFamilyIcon=" + crawlerFamilyIcon +
+                ", crawlerFamilyInfoUrl=" + crawlerFamilyInfoUrl +
+                ", crawlerLastSeen=" + crawlerLastSeen +
+                ", crawlerCategory=" + crawlerCategory +
+                ", crawlerCategoryCode=" + crawlerCategoryCode +
+                ", crawlerRespectRobotstxt=" + crawlerRespectRobotstxt +
+                ", dataCenterName=" + dataCenterName +
+                ", dataCenterNameCode=" + dataCenterNameCode +
+                ", dataCenterHomePage=" + dataCenterHomePage +
+                "]";
+    }
+
 }
--- src/main/java/org/udger/parser/UdgerUaResult.java
+++ src/main/java/org/udger/parser/UdgerUaResult.java
@@ -362,4 +362,351 @@ public class UdgerUaResult implements Serializable {
         this.deviceBrandInfoUrl = deviceBrandInfoUrl;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((classId == null) ? 0 : classId.hashCode());
+        result = prime * result + ((clientId == null) ? 0 : clientId.hashCode());
+        result = prime * result + ((crawlerCategory == null) ? 0 : crawlerCategory.hashCode());
+        result = prime * result + ((crawlerCategoryCode == null) ? 0 : crawlerCategoryCode.hashCode());
+        result = prime * result + ((crawlerLastSeen == null) ? 0 : crawlerLastSeen.hashCode());
+        result = prime * result + ((crawlerRespectRobotstxt == null) ? 0 : crawlerRespectRobotstxt.hashCode());
+        result = prime * result + ((deviceBrand == null) ? 0 : deviceBrand.hashCode());
+        result = prime * result + ((deviceBrandCode == null) ? 0 : deviceBrandCode.hashCode());
+        result = prime * result + ((deviceBrandHomepage == null) ? 0 : deviceBrandHomepage.hashCode());
+        result = prime * result + ((deviceBrandIcon == null) ? 0 : deviceBrandIcon.hashCode());
+        result = prime * result + ((deviceBrandIconBig == null) ? 0 : deviceBrandIconBig.hashCode());
+        result = prime * result + ((deviceBrandInfoUrl == null) ? 0 : deviceBrandInfoUrl.hashCode());
+        result = prime * result + ((deviceClass == null) ? 0 : deviceClass.hashCode());
+        result = prime * result + ((deviceClassCode == null) ? 0 : deviceClassCode.hashCode());
+        result = prime * result + ((deviceClassIcon == null) ? 0 : deviceClassIcon.hashCode());
+        result = prime * result + ((deviceClassIconBig == null) ? 0 : deviceClassIconBig.hashCode());
+        result = prime * result + ((deviceClassInfoUrl == null) ? 0 : deviceClassInfoUrl.hashCode());
+        result = prime * result + ((deviceMarketname == null) ? 0 : deviceMarketname.hashCode());
+        result = prime * result + ((os == null) ? 0 : os.hashCode());
+        result = prime * result + ((osCode == null) ? 0 : osCode.hashCode());
+        result = prime * result + ((osFamily == null) ? 0 : osFamily.hashCode());
+        result = prime * result + ((osFamilyCode == null) ? 0 : osFamilyCode.hashCode());
+        result = prime * result + ((osFamilyVedorHomepage == null) ? 0 : osFamilyVedorHomepage.hashCode());
+        result = prime * result + ((osFamilyVendor == null) ? 0 : osFamilyVendor.hashCode());
+        result = prime * result + ((osFamilyVendorCode == null) ? 0 : osFamilyVendorCode.hashCode());
+        result = prime * result + ((osHomePage == null) ? 0 : osHomePage.hashCode());
+        result = prime * result + ((osIcon == null) ? 0 : osIcon.hashCode());
+        result = prime * result + ((osIconBig == null) ? 0 : osIconBig.hashCode());
+        result = prime * result + ((osInfoUrl == null) ? 0 : osInfoUrl.hashCode());
+        result = prime * result + ((ua == null) ? 0 : ua.hashCode());
+        result = prime * result + ((uaClass == null) ? 0 : uaClass.hashCode());
+        result = prime * result + ((uaClassCode == null) ? 0 : uaClassCode.hashCode());
+        result = prime * result + ((uaEngine == null) ? 0 : uaEngine.hashCode());
+        result = prime * result + ((uaFamily == null) ? 0 : uaFamily.hashCode());
+        result = prime * result + ((uaFamilyCode == null) ? 0 : uaFamilyCode.hashCode());
+        result = prime * result + ((uaFamilyHomepage == null) ? 0 : uaFamilyHomepage.hashCode());
+        result = prime * result + ((uaFamilyIcon == null) ? 0 : uaFamilyIcon.hashCode());
+        result = prime * result + ((uaFamilyIconBig == null) ? 0 : uaFamilyIconBig.hashCode());
+        result = prime * result + ((uaFamilyInfoUrl == null) ? 0 : uaFamilyInfoUrl.hashCode());
+        result = prime * result + ((uaFamilyVendor == null) ? 0 : uaFamilyVendor.hashCode());
+        result = prime * result + ((uaFamilyVendorCode == null) ? 0 : uaFamilyVendorCode.hashCode());
+        result = prime * result + ((uaFamilyVendorHomepage == null) ? 0 : uaFamilyVendorHomepage.hashCode());
+        result = prime * result + ((uaString == null) ? 0 : uaString.hashCode());
+        result = prime * result + ((uaUptodateCurrentVersion == null) ? 0 : uaUptodateCurrentVersion.hashCode());
+        result = prime * result + ((uaVersion == null) ? 0 : uaVersion.hashCode());
+        result = prime * result + ((uaVersionMajor == null) ? 0 : uaVersionMajor.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UdgerUaResult other = (UdgerUaResult) obj;
+        if (classId == null) {
+            if (other.classId != null)
+                return false;
+        } else if (!classId.equals(other.classId))
+            return false;
+        if (clientId == null) {
+            if (other.clientId != null)
+                return false;
+        } else if (!clientId.equals(other.clientId))
+            return false;
+        if (crawlerCategory == null) {
+            if (other.crawlerCategory != null)
+                return false;
+        } else if (!crawlerCategory.equals(other.crawlerCategory))
+            return false;
+        if (crawlerCategoryCode == null) {
+            if (other.crawlerCategoryCode != null)
+                return false;
+        } else if (!crawlerCategoryCode.equals(other.crawlerCategoryCode))
+            return false;
+        if (crawlerLastSeen == null) {
+            if (other.crawlerLastSeen != null)
+                return false;
+        } else if (!crawlerLastSeen.equals(other.crawlerLastSeen))
+            return false;
+        if (crawlerRespectRobotstxt == null) {
+            if (other.crawlerRespectRobotstxt != null)
+                return false;
+        } else if (!crawlerRespectRobotstxt.equals(other.crawlerRespectRobotstxt))
+            return false;
+        if (deviceBrand == null) {
+            if (other.deviceBrand != null)
+                return false;
+        } else if (!deviceBrand.equals(other.deviceBrand))
+            return false;
+        if (deviceBrandCode == null) {
+            if (other.deviceBrandCode != null)
+                return false;
+        } else if (!deviceBrandCode.equals(other.deviceBrandCode))
+            return false;
+        if (deviceBrandHomepage == null) {
+            if (other.deviceBrandHomepage != null)
+                return false;
+        } else if (!deviceBrandHomepage.equals(other.deviceBrandHomepage))
+            return false;
+        if (deviceBrandIcon == null) {
+            if (other.deviceBrandIcon != null)
+                return false;
+        } else if (!deviceBrandIcon.equals(other.deviceBrandIcon))
+            return false;
+        if (deviceBrandIconBig == null) {
+            if (other.deviceBrandIconBig != null)
+                return false;
+        } else if (!deviceBrandIconBig.equals(other.deviceBrandIconBig))
+            return false;
+        if (deviceBrandInfoUrl == null) {
+            if (other.deviceBrandInfoUrl != null)
+                return false;
+        } else if (!deviceBrandInfoUrl.equals(other.deviceBrandInfoUrl))
+            return false;
+        if (deviceClass == null) {
+            if (other.deviceClass != null)
+                return false;
+        } else if (!deviceClass.equals(other.deviceClass))
+            return false;
+        if (deviceClassCode == null) {
+            if (other.deviceClassCode != null)
+                return false;
+        } else if (!deviceClassCode.equals(other.deviceClassCode))
+            return false;
+        if (deviceClassIcon == null) {
+            if (other.deviceClassIcon != null)
+                return false;
+        } else if (!deviceClassIcon.equals(other.deviceClassIcon))
+            return false;
+        if (deviceClassIconBig == null) {
+            if (other.deviceClassIconBig != null)
+                return false;
+        } else if (!deviceClassIconBig.equals(other.deviceClassIconBig))
+            return false;
+        if (deviceClassInfoUrl == null) {
+            if (other.deviceClassInfoUrl != null)
+                return false;
+        } else if (!deviceClassInfoUrl.equals(other.deviceClassInfoUrl))
+            return false;
+        if (deviceMarketname == null) {
+            if (other.deviceMarketname != null)
+                return false;
+        } else if (!deviceMarketname.equals(other.deviceMarketname))
+            return false;
+        if (os == null) {
+            if (other.os != null)
+                return false;
+        } else if (!os.equals(other.os))
+            return false;
+        if (osCode == null) {
+            if (other.osCode != null)
+                return false;
+        } else if (!osCode.equals(other.osCode))
+            return false;
+        if (osFamily == null) {
+            if (other.osFamily != null)
+                return false;
+        } else if (!osFamily.equals(other.osFamily))
+            return false;
+        if (osFamilyCode == null) {
+            if (other.osFamilyCode != null)
+                return false;
+        } else if (!osFamilyCode.equals(other.osFamilyCode))
+            return false;
+        if (osFamilyVedorHomepage == null) {
+            if (other.osFamilyVedorHomepage != null)
+                return false;
+        } else if (!osFamilyVedorHomepage.equals(other.osFamilyVedorHomepage))
+            return false;
+        if (osFamilyVendor == null) {
+            if (other.osFamilyVendor != null)
+                return false;
+        } else if (!osFamilyVendor.equals(other.osFamilyVendor))
+            return false;
+        if (osFamilyVendorCode == null) {
+            if (other.osFamilyVendorCode != null)
+                return false;
+        } else if (!osFamilyVendorCode.equals(other.osFamilyVendorCode))
+            return false;
+        if (osHomePage == null) {
+            if (other.osHomePage != null)
+                return false;
+        } else if (!osHomePage.equals(other.osHomePage))
+            return false;
+        if (osIcon == null) {
+            if (other.osIcon != null)
+                return false;
+        } else if (!osIcon.equals(other.osIcon))
+            return false;
+        if (osIconBig == null) {
+            if (other.osIconBig != null)
+                return false;
+        } else if (!osIconBig.equals(other.osIconBig))
+            return false;
+        if (osInfoUrl == null) {
+            if (other.osInfoUrl != null)
+                return false;
+        } else if (!osInfoUrl.equals(other.osInfoUrl))
+            return false;
+        if (ua == null) {
+            if (other.ua != null)
+                return false;
+        } else if (!ua.equals(other.ua))
+            return false;
+        if (uaClass == null) {
+            if (other.uaClass != null)
+                return false;
+        } else if (!uaClass.equals(other.uaClass))
+            return false;
+        if (uaClassCode == null) {
+            if (other.uaClassCode != null)
+                return false;
+        } else if (!uaClassCode.equals(other.uaClassCode))
+            return false;
+        if (uaEngine == null) {
+            if (other.uaEngine != null)
+                return false;
+        } else if (!uaEngine.equals(other.uaEngine))
+            return false;
+        if (uaFamily == null) {
+            if (other.uaFamily != null)
+                return false;
+        } else if (!uaFamily.equals(other.uaFamily))
+            return false;
+        if (uaFamilyCode == null) {
+            if (other.uaFamilyCode != null)
+                return false;
+        } else if (!uaFamilyCode.equals(other.uaFamilyCode))
+            return false;
+        if (uaFamilyHomepage == null) {
+            if (other.uaFamilyHomepage != null)
+                return false;
+        } else if (!uaFamilyHomepage.equals(other.uaFamilyHomepage))
+            return false;
+        if (uaFamilyIcon == null) {
+            if (other.uaFamilyIcon != null)
+                return false;
+        } else if (!uaFamilyIcon.equals(other.uaFamilyIcon))
+            return false;
+        if (uaFamilyIconBig == null) {
+            if (other.uaFamilyIconBig != null)
+                return false;
+        } else if (!uaFamilyIconBig.equals(other.uaFamilyIconBig))
+            return false;
+        if (uaFamilyInfoUrl == null) {
+            if (other.uaFamilyInfoUrl != null)
+                return false;
+        } else if (!uaFamilyInfoUrl.equals(other.uaFamilyInfoUrl))
+            return false;
+        if (uaFamilyVendor == null) {
+            if (other.uaFamilyVendor != null)
+                return false;
+        } else if (!uaFamilyVendor.equals(other.uaFamilyVendor))
+            return false;
+        if (uaFamilyVendorCode == null) {
+            if (other.uaFamilyVendorCode != null)
+                return false;
+        } else if (!uaFamilyVendorCode.equals(other.uaFamilyVendorCode))
+            return false;
+        if (uaFamilyVendorHomepage == null) {
+            if (other.uaFamilyVendorHomepage != null)
+                return false;
+        } else if (!uaFamilyVendorHomepage.equals(other.uaFamilyVendorHomepage))
+            return false;
+        if (uaString == null) {
+            if (other.uaString != null)
+                return false;
+        } else if (!uaString.equals(other.uaString))
+            return false;
+        if (uaUptodateCurrentVersion == null) {
+            if (other.uaUptodateCurrentVersion != null)
+                return false;
+        } else if (!uaUptodateCurrentVersion.equals(other.uaUptodateCurrentVersion))
+            return false;
+        if (uaVersion == null) {
+            if (other.uaVersion != null)
+                return false;
+        } else if (!uaVersion.equals(other.uaVersion))
+            return false;
+        if (uaVersionMajor == null) {
+            if (other.uaVersionMajor != null)
+                return false;
+        } else if (!uaVersionMajor.equals(other.uaVersionMajor))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "UdgerUaResult [" +
+                "uaString=" + uaString +
+                ", clientId=" + clientId +
+                ", classId=" + classId +
+                ", uaClass=" + uaClass +
+                ", uaClassCode=" + uaClassCode +
+                ", ua=" + ua +
+                ", uaEngine=" + uaEngine +
+                ", uaVersion=" + uaVersion +
+                ", uaVersionMajor=" + uaVersionMajor +
+                ", crawlerLastSeen=" + crawlerLastSeen +
+                ", crawlerRespectRobotstxt=" + crawlerRespectRobotstxt +
+                ", crawlerCategory=" + crawlerCategory +
+                ", crawlerCategoryCode=" + crawlerCategoryCode +
+                ", uaUptodateCurrentVersion=" + uaUptodateCurrentVersion +
+                ", uaFamily=" + uaFamily +
+                ", uaFamilyCode=" + uaFamilyCode +
+                ", uaFamilyHomepage=" + uaFamilyHomepage +
+                ", uaFamilyIcon=" + uaFamilyIcon +
+                ", uaFamilyIconBig=" + uaFamilyIconBig +
+                ", uaFamilyVendor=" + uaFamilyVendor +
+                ", uaFamilyVendorCode=" + uaFamilyVendorCode +
+                ", uaFamilyVendorHomepage=" + uaFamilyVendorHomepage +
+                ", uaFamilyInfoUrl=" + uaFamilyInfoUrl +
+                ", osFamily=" + osFamily +
+                ", osFamilyCode=" + osFamilyCode +
+                ", os=" + os +
+                ", osCode=" + osCode +
+                ", osHomePage=" + osHomePage +
+                ", osIcon=" + osIcon +
+                ", osIconBig=" + osIconBig +
+                ", osFamilyVendor=" + osFamilyVendor +
+                ", osFamilyVendorCode=" + osFamilyVendorCode +
+                ", osFamilyVedorHomepage=" + osFamilyVedorHomepage +
+                ", osInfoUrl=" + osInfoUrl +
+                ", deviceClass=" + deviceClass +
+                ", deviceClassCode=" + deviceClassCode +
+                ", deviceClassIcon=" + deviceClassIcon +
+                ", deviceClassIconBig=" + deviceClassIconBig +
+                ", deviceClassInfoUrl=" + deviceClassInfoUrl +
+                ", deviceMarketname=" + deviceMarketname +
+                ", deviceBrand=" + deviceBrand +
+                ", deviceBrandCode=" + deviceBrandCode +
+                ", deviceBrandHomepage=" + deviceBrandHomepage +
+                ", deviceBrandIcon=" + deviceBrandIcon +
+                ", deviceBrandIconBig=" + deviceBrandIconBig +
+                ", deviceBrandInfoUrl=" + deviceBrandInfoUrl +
+                "]";
+    }
+
 }
